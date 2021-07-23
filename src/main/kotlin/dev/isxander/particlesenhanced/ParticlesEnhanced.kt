30373b75b321b2c5922c7e0ded3b6b8c7c284010
Xander <xandersmith2008@gmail.com>
2021-07-23 16:50:52 +0100

    add gui options edit

--- src/main/kotlin/dev/isxander/particlesenhanced/ParticlesEnhanced.kt
+++ src/main/kotlin/dev/isxander/particlesenhanced/ParticlesEnhanced.kt
@@ -11,8 +11,13 @@ import dev.isxander.particlesenhanced.config.ParticlesEnhancedConfig
 import dev.isxander.particlesenhanced.modules.overrides.AlwaysCriticals
 import dev.isxander.particlesenhanced.modules.overrides.AlwaysSharpness
 import dev.isxander.xanderlib.XanderLib
+import dev.isxander.xanderlib.ui.editor.AbstractGuiModifier
 import dev.isxander.xanderlib.utils.Constants.mc
+import gg.essential.api.EssentialAPI
 import gg.essential.vigilance.Vigilance
+import net.minecraft.client.gui.GuiButton
+import net.minecraft.client.gui.GuiOptions
+import net.minecraft.client.gui.GuiScreen
 import net.minecraftforge.common.MinecraftForge
 import net.minecraftforge.fml.common.Mod
 import net.minecraftforge.fml.common.event.FMLInitializationEvent
@@ -48,6 +53,22 @@ object ParticlesEnhanced {
 
         MinecraftForge.EVENT_BUS.register(AlwaysCriticals)
         MinecraftForge.EVENT_BUS.register(AlwaysSharpness)
+
+        XanderLib.getInstance().guiEditor.addModifier(GuiOptions::class.java, object : AbstractGuiModifier {
+            override fun onInitGuiPost(screen: GuiScreen, buttonList: MutableList<GuiButton>) {
+                buttonList.removeIf { it.id == 107 }
+                buttonList.add(GuiButton(838573356, screen.width / 2 + 5, screen.height / 6 + 72 - 6, 150, 20, "Particles Enhanced..."))
+            }
+
+            override fun onActionPerformedPost(
+                screen: GuiScreen,
+                buttonList: MutableList<GuiButton>,
+                button: GuiButton
+            ) {
+                if (button.id != 838573356) return
+                mc.displayGuiScreen(ParticlesEnhancedConfig.gui())
+            }
+        })
     }
 
 }
