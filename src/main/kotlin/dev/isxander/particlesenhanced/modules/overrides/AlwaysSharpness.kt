47979fe3b865e8065b18f63c52d001a288b528b8
Xander <xandersmith2008@gmail.com>
2021-07-24 10:17:30 +0100

    add options to change the type of particle on crit and sharp. fixed invulnerability check

--- src/main/kotlin/dev/isxander/particlesenhanced/modules/overrides/AlwaysSharpness.kt
+++ src/main/kotlin/dev/isxander/particlesenhanced/modules/overrides/AlwaysSharpness.kt
@@ -7,10 +7,15 @@
 package dev.isxander.particlesenhanced.modules.overrides
 
 import dev.isxander.particlesenhanced.config.ParticlesEnhancedConfig
+import dev.isxander.xanderlib.event.PacketEvent
 import dev.isxander.xanderlib.utils.Constants
+import dev.isxander.xanderlib.utils.Constants.mc
 import net.minecraft.enchantment.EnchantmentHelper
+import net.minecraft.entity.Entity
 import net.minecraft.entity.EntityLivingBase
 import net.minecraft.entity.EnumCreatureAttribute
+import net.minecraft.entity.player.EntityPlayer
+import net.minecraft.network.play.server.S19PacketEntityStatus
 import net.minecraft.util.DamageSource
 import net.minecraft.util.EnumParticleTypes
 import net.minecraftforge.event.entity.player.AttackEntityEvent
@@ -18,24 +23,50 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 
 object AlwaysSharpness {
 
+    private var attacker: EntityPlayer? = null
+    private var targetId = -1
+
+    @SubscribeEvent
+    fun onPacket(event: PacketEvent.Incoming) {
+        if (!ParticlesEnhancedConfig.checkInvulnerable) return
+
+        if (event.packet is S19PacketEntityStatus) {
+            val packet = event.packet as S19PacketEntityStatus
+            if (packet.opCode.toInt() != 2) return
+
+            val target = packet.getEntity(Constants.mc.theWorld) ?: return
+            if (attacker != null && targetId == target.entityId) {
+                doSharpness(attacker!!, target)
+                attacker = null
+                targetId = -1
+            }
+        }
+    }
+
     @SubscribeEvent
     fun onAttack(event: AttackEntityEvent) {
+        if (ParticlesEnhancedConfig.checkInvulnerable) {
+            if (event.entityPlayer.entityId == Constants.mc.thePlayer.entityId) {
+                attacker = event.entityPlayer
+                targetId = event.target.entityId
+            }
+        } else {
+            doSharpness(event.entityPlayer, event.target)
+        }
+    }
+
+    fun doSharpness(attacker: EntityPlayer, target: Entity) {
         if (!ParticlesEnhancedConfig.alwaysSharp)
             return
 
-        if (ParticlesEnhancedConfig.checkInvulnerable && event.target.isEntityInvulnerable(DamageSource.causePlayerDamage(event.entityPlayer)))
-            return
-
-        val target = event.target
-
         val modifier = if (target is EntityLivingBase) {
-            EnchantmentHelper.getModifierForCreature(event.entityLiving.heldItem, target.creatureAttribute)
+            EnchantmentHelper.getModifierForCreature(attacker.heldItem, target.creatureAttribute)
         } else {
-            EnchantmentHelper.getModifierForCreature(event.entityLiving.heldItem, EnumCreatureAttribute.UNDEFINED)
+            EnchantmentHelper.getModifierForCreature(attacker.heldItem, EnumCreatureAttribute.UNDEFINED)
         }
 
         if (modifier <= 0f) {
-            Constants.mc.effectRenderer.emitParticleAtEntity(event.target, EnumParticleTypes.CRIT_MAGIC)
+            mc.effectRenderer.emitParticleAtEntity(target, EnumParticleTypes.CRIT_MAGIC)
         }
     }
 
